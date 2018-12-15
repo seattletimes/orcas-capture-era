c4cfd35b17021ad77d06e24f2f6ef1208d5d7c2b
sfmina <fmina@seattletimes.com>
2018-12-14 16:56:59 -0800

    published

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+require("./lib/ads");
+var paywall = require("./lib/paywall");
+setTimeout(() => paywall(11176774), 3000);
+
+var track = require("./lib/tracking");
+
+require("component-responsive-frame/child");
+
+require("./bc-preroll");
+
